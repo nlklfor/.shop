f70387717cd9c3f8e7ef40a5a754b7b4d283cb4a
Nikita <nik.slepcov1771@gmail.com>
2021-12-15 18:47:13 +0200

    finished cart

--- js/cart.js
+++ js/cart.js
@@ -4,6 +4,7 @@ const cart = function(){
     const cartCls = document.querySelector('.modal-close');
     const goodsContainer = document.querySelector('.long-goods-list');
     const cartTableGoods = document.querySelector('.cart-table__goods');
+    const modalForm = document.querySelector('.modal-form');
 
     const deleteCartItem = function(id){
         const cart = JSON.parse(localStorage.getItem('cart'))
@@ -12,19 +13,38 @@ const cart = function(){
             return good.id !== id;
         })
 
-        localStorage.setItem('cart', JSON.stringify(newCart))
+        localStorage.setItem('cart', JSON.stringify(newCart));
         renderCartGoods(JSON.parse(localStorage.getItem('cart')));
     }
     const plusCartItem = function(id){
         const cart = JSON.parse(localStorage.getItem('cart'))
 
-        localStorage.setItem('cart', JSON.stringify(cart))
+        const newCart = cart.map(good => {
+            if(good.id === id){
+              good.count++
+            }
+            return good;
+        })
+
+        localStorage.setItem('cart', JSON.stringify(newCart));
+        renderCartGoods(JSON.parse(localStorage.getItem('cart')));
         // renderCartFoods();
     }
     const minusCartItem = function(id){
         const cart = JSON.parse(localStorage.getItem('cart'))
 
-        localStorage.setItem('cart', JSON.stringify(cart))
+        const newCart = cart.map(good => {
+            if(good.id === id){
+                if(good.count > 0){
+                    good.count--
+                }
+              
+            }
+            return good;
+        })
+
+        localStorage.setItem('cart', JSON.stringify(newCart));
+        renderCartGoods(JSON.parse(localStorage.getItem('cart')));
         // renderCartFoods();
     }
     const addToCart = function(id){
@@ -66,15 +86,37 @@ const cart = function(){
 
             tr.addEventListener('click', function(event){
                 if (event.target.classList.contains('cart-btn-minus')){
-
+                    minusCartItem(good.id);
                 }else if (event.target.classList.contains('cart-btn-plus')){
-
+                    plusCartItem(good.id);
                 }else if(event.target.classList.contains('cart-btn-delete')) {
                     deleteCartItem(good.id);
                 }
             })
         })
     }
+
+
+    const sendForm = function(){
+        const cartArray = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : []
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify({
+                cart: cartArray,
+                name: '',
+                phone: ''
+            })
+        }).then(function(){
+            cart.style.display="none";
+        })
+    } 
+
+    modalForm.addEventListener('submit' , function(event){
+        sendForm();
+        event.preventDefault();
+        
+    })
+
 
     cartBtn.addEventListener('click', function(){
         const cartArray = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : []
